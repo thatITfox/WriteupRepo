432f153d60a5791040d5d2811e1defa0bdd2af87
thatITfox <maxthecomputerfox@gmail.com>
2026-05-10 20:07:44 +0700

    Finished basic user interface

--- static/main.js
+++ static/main.js
@@ -1,2 +1,8 @@
-// TODO: set theme for every pre code tags
+// The code below makes every code that will be rendered by highlight.js
+// use the atom-one-dark theme.
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('pre').forEach(el => {
+        el.classList.add('theme-atom-one-dark');
+    });
+});
